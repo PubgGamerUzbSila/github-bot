0b2a73236c38a8227e0baca32f9cff332cd1f4aa
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2024-03-22 15:46:03 +0500

    2024-03-22T15:46:03+05:00

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const moment = require("moment");
 const simpleGit = require("simple-git");
 const FILE_PATH = "./data.json";
 
-const makeCommit =n=>{
-    if(n===0) return simpleGit().push();
-  const x = 1;
-  const y = 3;
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
+  const x = Math.random() * 100;
+  const y = Math.random() * 100;
   const DATE = moment()
     .subtract(1, "y")
     .add(1, "d")
@@ -19,8 +19,9 @@ const makeCommit =n=>{
   };
   console.log(DATE);
   jsonfile.writeFile(FILE_PATH, data, () => {
-    simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE }, makeCommit.bind(this,--n));
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
   });
 };
-
 makeCommit(100);
